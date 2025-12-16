5ebb11a241c765c034c367e234289eb93adad624
TbangIT <76477475+TbangIT@users.noreply.github.com>
2025-12-16 16:15:37 +0100

    Update greenboot.sh

--- build_files/greenboot.sh
+++ build_files/greenboot.sh
@@ -8,4 +8,13 @@ PKGS=(
 
 DNF "${PKGS[@]}"
 
-systemctl enable greenboot-task-runner greenboot-healthcheck greenboot-status greenboot-loading-message greenboot-grub2-set-counter greenboot-grub2-set-success greenboot-rpm-ostree-grub2-check-fallback redboot-auto-reboot redboot-task-runner
+if [ -d /run/systemd/system ]; then
+  systemctl enable \
+    greenboot-healthcheck \
+    greenboot-status \
+    greenboot-loading-message \
+    greenboot-grub2-set-counter \
+    greenboot-grub2-set-success \
+    greenboot-rpm-ostree-grub2-check-fallback \
+    redboot-auto-reboot
+fi
